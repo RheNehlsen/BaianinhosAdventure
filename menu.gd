a9361e05e8efdd214f89015f2daeb97d828c5421
Pedro Teles <pedroptelez@gmail.com>
2023-07-04 00:04:54 -0300

    :construction: feat: starting game with enter

--- menu.gd
+++ menu.gd
@@ -3,15 +3,16 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/StartButton.grab_focus()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("enter"):
+		get_tree().change_scene_to_file("res://copia_main.tscn")
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://copia_main.tscn")
+	pass
 
 
 func _on_quit_button_pressed():
